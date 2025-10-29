fe5b1cd1bb0537059b40b9bd6c7c946b3397a6f1
GrayNguyen77 <ndtoan.work@gmail.com>
2025-10-29 11:32:55 +0700

    add load and try catch messages

--- scripts/load_full_data.sql
+++ scripts/load_full_data.sql
@@ -1,8 +1,14 @@
 
+CALL bronze.load_bronze();
+
 CREATE OR REPLACE PROCEDURE bronze.load_bronze()
 AS $$
+DECLARE
+	start_time TIMESTAMP;
+	end_time TIMESTAMP;
+	duration NUMERIC;
 BEGIN 
-
+	BEGIN
 	RAISE NOTICE '================================================================';
 	RAISE NOTICE 'Loading Bronze Layer ';
 	RAISE NOTICE '================================================================';
@@ -11,6 +17,7 @@ BEGIN
 	RAISE NOTICE 'Loading CRM Tables';
 	RAISE NOTICE '----------------------------------------------------------------';
 
+	start_time := clock_timestamp();
 	RAISE NOTICE '>>>TRUNCATE TABLE: bronze.crm_cust_info';
 	TRUNCATE TABLE bronze.crm_cust_info;
 	RAISE NOTICE '>>>INSERTING DATA INTO : bronze.crm_cust_info';
@@ -18,7 +25,12 @@ BEGIN
 	FROM 'D:\Project_Portfolio\SQL Data Warehouse\dataset\source_crm\cust_info.csv'
 	DELIMITER ','
 	CSV HEADER;
+	end_time := clock_timestamp();
+	duration = EXTRACT(EPOCH FROM (end_time - start_time));
+	RAISE NOTICE '>>>LOAD DURATION bronze.crm_cust_info: % seconds',duration;
+	RAISE NOTICE '----------------------------------------------------------------';
 
+	start_time := clock_timestamp();
 	RAISE NOTICE '>>>TRUNCATE TABLE: bronze.crm_prd_info';
 	TRUNCATE TABLE bronze.crm_prd_info;
 	RAISE NOTICE '>>>INSERTING DATA INTO : bronze.crm_prd_info';
@@ -26,7 +38,12 @@ BEGIN
 	FROM 'D:\Project_Portfolio\SQL Data Warehouse\dataset\source_crm\prd_info.csv'
 	DELIMITER ','
 	CSV HEADER;
+	end_time := clock_timestamp();
+	duration = EXTRACT(EPOCH FROM (end_time - start_time));
+	RAISE NOTICE '>>>LOAD DURATION bronze.crm_prd_info: % seconds',duration;
+	RAISE NOTICE '----------------------------------------------------------------';
 
+	start_time := clock_timestamp();
 	RAISE NOTICE '>>>TRUNCATE TABLE: bronze.crm_sales_details';
 	TRUNCATE TABLE bronze.crm_sales_details;
 	RAISE NOTICE '>>>INSERTING DATA INTO : bronze.crm_sales_details';
@@ -35,11 +52,16 @@ BEGIN
 	DELIMITER ','
 	CSV HEADER
 	NULL '';
-
+	end_time := clock_timestamp();
+	duration = EXTRACT(EPOCH FROM (end_time - start_time));
+	RAISE NOTICE '>>>LOAD DURATION bronze.crm_sales_details: % seconds',duration;
+	RAISE NOTICE '----------------------------------------------------------------';
+	
 	RAISE NOTICE '----------------------------------------------------------------';
 	RAISE NOTICE 'Loading ERP Tables';
 	RAISE NOTICE '----------------------------------------------------------------';
 
+	start_time := clock_timestamp();
 	RAISE NOTICE '>>>TRUNCATE TABLE: bronze.erp_cust_az1';
 	TRUNCATE TABLE bronze.erp_cust_az12;
 	RAISE NOTICE '>>>INSERTING DATA INTO : bronze.erp_cust_az12';
@@ -47,7 +69,12 @@ BEGIN
 	FROM 'D:\Project_Portfolio\SQL Data Warehouse\dataset\source_erp\cust_az12.csv'
 	DELIMITER ','
 	CSV HEADER;
+	end_time := clock_timestamp();
+	duration = EXTRACT(EPOCH FROM (end_time - start_time));
+	RAISE NOTICE '>>>LOAD DURATION bronze.crm_sales_details: % seconds',duration;
+	RAISE NOTICE '----------------------------------------------------------------';
 
+	start_time := clock_timestamp();
 	RAISE NOTICE '>>>TRUNCATE TABLE: bronze.erp_loc_a1011';
 	TRUNCATE TABLE bronze.erp_loc_a101;
 	RAISE NOTICE '>>>INSERTING DATA INTO : bronze.erp_loc_a1011';
@@ -55,7 +82,12 @@ BEGIN
 	FROM 'D:\Project_Portfolio\SQL Data Warehouse\dataset\source_erp\loc_a101.csv'
 	DELIMITER ','
 	CSV HEADER;
+	end_time := clock_timestamp();
+	duration = EXTRACT(EPOCH FROM (end_time - start_time));
+	RAISE NOTICE '>>>LOAD DURATION bronze.crm_sales_details: % seconds',duration;
+	RAISE NOTICE '----------------------------------------------------------------';
 
+	start_time := clock_timestamp();
 	RAISE NOTICE '>>>TRUNCATE TABLE: bronze.erp_px_cat_g1v2';
 	TRUNCATE TABLE bronze.erp_px_cat_g1v2;
 	RAISE NOTICE '>>>INSERTING DATA INTO : bronze.erp_px_cat_g1v2';
@@ -63,11 +95,22 @@ BEGIN
 	FROM 'D:\Project_Portfolio\SQL Data Warehouse\dataset\source_erp\PX_CAT_G1V2.csv'
 	DELIMITER ','
 	CSV HEADER;
+	end_time := clock_timestamp();
+	duration = EXTRACT(EPOCH FROM (end_time - start_time));
+	RAISE NOTICE '>>>LOAD DURATION bronze.crm_sales_details: % seconds',duration;
+	RAISE NOTICE '----------------------------------------------------------------';
 
 	RAISE NOTICE '>>>----------COMPLETE----------<<<';
+	EXCEPTION
+		WHEN others THEN
+			RAISE NOTICE '==================================================================';
+			RAISE NOTICE 'ERROR OCCURED DURING LOADING BRONZE';
+			RAISE NOTICE 'ERROR MESSAGE: %', SQLERRM;
+			RAISE NOTICE '==================================================================';
+	END;
 END;
 $$
 LANGUAGE plpgsql;
 
-CALL bronze.load_bronze()
+
 
